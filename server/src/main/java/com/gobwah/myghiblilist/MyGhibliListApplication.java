4bf8e32ba1e9e93dff1c9b14a752b928abe163e1
Vincent Della Libera <supervince40@hotmail.fr>
2022-03-23 21:08:18 +0100

    Add backend

---
+++ server/src/main/java/com/gobwah/myghiblilist/MyGhibliListApplication.java
@@ -0,0 +1,13 @@
+package com.gobwah.myghiblilist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGhibliListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGhibliListApplication.class, args);
+	}
+
+}
